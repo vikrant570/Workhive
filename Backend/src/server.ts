9469df1bd19142cdb7aa0d433e45c289cff86bdf
Vikrant Singh <thevikrantsinghofficial@gmail.com>
2026-07-25 18:52:46 +0530

    Reconfigured the env varibales

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -4,11 +4,15 @@ import mongoose from "mongoose";
 import { initWebSocket } from "./websockets/webSocketSetup";
 
 const server = http.createServer(app);
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const DB = process.env.DB_URL;
+
+if (!DB || !PORT) {
+    throw new Error("Please provide DB URL and PORT");
+}
 
 initWebSocket(server)
 
 server.listen(PORT, async () => {
-    await mongoose.connect(`${process.env.DB_URL}`);
-    console.log(`Server started at http://localhost:${PORT}`)
+    await mongoose.connect(DB);
 })
